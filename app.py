ccac4dcca4ea0570fa3267afed91d5718f7ea583
alya-atm <55447254+alya-atm@users.noreply.github.com>
2019-11-25 00:44:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from views.create_entry import create_entry_page
 from views.entry import entry_page
 app = Flask(__name__)
 
-app.add_url_rule('/', view_func=index_page, methods=['GET', 'POST'])
+app.add_url_rule('/', view_func=index_page, methods=[ 'POST'])
 app.add_url_rule('/entry/<key>', view_func=entry_page,methods=['GET','POST'])
 app.add_url_rule('/create', view_func=create_entry_page,methods=['GET','POST'])
 
